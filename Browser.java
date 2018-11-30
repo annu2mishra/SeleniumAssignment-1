91646d00a5d730880004ad548d3fdc88740585d2
annu2mishra <45196456+annu2mishra@users.noreply.github.com>
2018-11-30 16:07:31 +0530

    Add files via upload

---
+++ Browser.java
@@ -0,0 +1,34 @@
+package com.ibm.SeleniumConcepts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		System.setProperty("webdriver.ie.driver","./driver/IEDriverServer.exe");
+				
+				//WebDriver driver= new ChromeDriver();
+				WebDriver driver= new InternetExplorerDriver();
+		          
+		       
+                
+                
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+
+}
